c661b78dc1c70d3e87b4a9789461945a8390ad8a
starbvuks <starbvuks@gmail.com>
2021-07-11 18:49:55 +0530

    dispatch-actior

--- fcc-redux-snippets/Snippets.js
+++ fcc-redux-snippets/Snippets.js
@@ -29,10 +29,28 @@ const action = {
 
 // ## Define an Action Creator
 
-/*An action creator is simply a JavaScript function that returns an action. 
+/* An action creator is simply a JavaScript function that returns an action. 
 In other words, action creators create objects that represent action events.
 */
 
 const actionCreator = () => {
   return action;
 };
+
+// ## Dispatch an Action Event
+
+/* Dispatch method is what you use to dispatch actions to the Redux store. 
+Calling store.dispatch() and passing the value returned from an action creator 
+sends an action back to the store.
+*/
+
+const store = Redux.createStore((state = {login: false}) => state);
+
+const loginAction = () => {
+  return {
+    type: "LOGIN",
+  };
+};
+
+// Dispatch the action here:
+store.dispatch(loginAction());
