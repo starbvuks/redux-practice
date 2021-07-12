be2718ad7a19f63e9940a7984a17edd76027a717
starbvuks <starbvuks@gmail.com>
2021-07-12 09:18:33 +0530

    subcscribe

--- fcc-redux-snippets/Snippets.js
+++ fcc-redux-snippets/Snippets.js
@@ -160,3 +160,41 @@ const logoutUser = () => {
     type: LOGOUT,
   };
 };
+
+// ## Register a Store Listener
+
+/* Another method you have access to on the Redux store object is store.subscribe(). 
+This allows you to subscribe listener functions to the store, which are called whenever 
+an action is dispatched against the store.
+*/
+
+const ADD = "ADD";
+
+const reducer = (state = 0, action) => {
+  switch (action.type) {
+    case ADD:
+      return state + 1;
+    default:
+      return state;
+  }
+};
+
+const store = Redux.createStore(reducer);
+
+// Global count variable:
+let count = 0;
+
+// callback func + subscribe
+const increment = () => {
+  return count++;
+};
+
+store.subscribe(increment);
+// callback func + subscribe
+
+store.dispatch({type: ADD});
+console.log(count);
+store.dispatch({type: ADD});
+console.log(count);
+store.dispatch({type: ADD});
+console.log(count);
