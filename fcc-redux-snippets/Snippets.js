2a37f6d98b2a702cad814c1ef9e2f6461c223efb
starbvuks <starbvuks@gmail.com>
2021-07-12 11:32:26 +0530

    counter

--- fcc-redux-snippets/Snippets.js
+++ fcc-redux-snippets/Snippets.js
@@ -338,3 +338,38 @@ const store = Redux.createStore(
   asyncDataReducer,
   Redux.applyMiddleware(ReduxThunk.default)
 );
+
+// ## Redux Counter
+
+const INCREMENT = "INCREMENT"; // Define a constant for increment action types
+const DECREMENT = "DECREMENT"; // Define a constant for decrement action types
+
+const counter = 0;
+
+const counterReducer = (state = counter, action) => {
+  switch (action.type) {
+    case INCREMENT:
+      return state + 1;
+      break;
+    case DECREMENT:
+      return state - 1;
+      break;
+    default:
+      return state;
+  }
+};
+// Define the counter reducer which will increment or decrement the state based on the action it receives
+
+const incAction = () => {
+  return {
+    type: INCREMENT,
+  };
+}; // Define an action creator for incrementing
+
+const decAction = () => {
+  return {
+    type: DECREMENT,
+  };
+}; // Define an action creator for decrementing
+
+const store = Redux.createStore(counterReducer); // Define the Redux store here, passing in your reducers
