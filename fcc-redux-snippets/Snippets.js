e893abcacfecee9643dcb757763cbfeee8117b33
starbvuks <starbvuks@gmail.com>
2021-07-12 09:29:09 +0530

    root-reducer

--- fcc-redux-snippets/Snippets.js
+++ fcc-redux-snippets/Snippets.js
@@ -198,3 +198,49 @@ store.dispatch({type: ADD});
 console.log(count);
 store.dispatch({type: ADD});
 console.log(count);
+
+// ## Combine Multiple Reducers
+
+/* Define multiple reducers to handle different pieces of your application's state, 
+then compose these reducers together into one root reducer
+*/
+
+const INCREMENT = "INCREMENT";
+const DECREMENT = "DECREMENT";
+
+const counterReducer = (state = 0, action) => {
+  switch (action.type) {
+    case INCREMENT:
+      return state + 1;
+    case DECREMENT:
+      return state - 1;
+    default:
+      return state;
+  }
+};
+
+const LOGIN = "LOGIN";
+const LOGOUT = "LOGOUT";
+
+const authReducer = (state = {authenticated: false}, action) => {
+  switch (action.type) {
+    case LOGIN:
+      return {
+        authenticated: true,
+      };
+    case LOGOUT:
+      return {
+        authenticated: false,
+      };
+    default:
+      return state;
+  }
+};
+
+// Root reducer:
+const rootReducer = Redux.combineReducers({
+  count: counterReducer,
+  auth: authReducer,
+});
+
+const store = Redux.createStore(rootReducer);
